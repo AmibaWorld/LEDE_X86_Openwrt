405a43d9fbe667988579de24b7d0702a4056cbe1
AmibaWorld <34524850+AmibaWorld@users.noreply.github.com>
2022-05-09 20:18:34 +0800

    Update diy-part2.sh
    修改内核版本

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,6 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.100/g' package/base-files/files/bin/config_generate
 
